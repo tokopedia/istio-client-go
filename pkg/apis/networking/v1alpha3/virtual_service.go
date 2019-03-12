169bd31324774547c780583d72b3718026542c69
Ahmad Muzakki <muzakki.ahmad29@gmail.com>
2019-03-12 19:20:41 +0700

    add istio virtualservice type

--- pkg/apis/networking/v1alpha3/virtual_service.go
+++ pkg/apis/networking/v1alpha3/virtual_service.go
@@ -15,8 +15,6 @@ limitations under the License.
 package v1alpha3
 
 import (
-	"github.com/golang/protobuf/proto"
-	istiov1alpha3 "istio.io/api/networking/v1alpha3"
 	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
 )
 
@@ -32,10 +30,6 @@ type VirtualService struct {
 	Spec VirtualServiceSpec `json:"spec"`
 }
 
-func (vs *VirtualService) GetSpecMessage() proto.Message {
-	return &vs.Spec.VirtualService
-}
-
 // +k8s:deepcopy-gen:interfaces=k8s.io/apimachinery/pkg/runtime.Object
 
 // VirtualServiceList is a list of VirtualService resources
@@ -46,13 +40,156 @@ type VirtualServiceList struct {
 	Items []VirtualService `json:"items"`
 }
 
-// VirtualServiceSpec is a wrapper around Istio VirtualService
-type VirtualServiceSpec struct {
-	istiov1alpha3.VirtualService
-}
-
 // DeepCopyInto is a deepcopy function, copying the receiver, writing into out. in must be non-nil.
 // Based of https://github.com/istio/istio/blob/release-0.8/pilot/pkg/config/kube/crd/types.go#L450
 func (in *VirtualServiceSpec) DeepCopyInto(out *VirtualServiceSpec) {
 	*out = *in
+}
+
+type VirtualServiceSpec struct {
+	Hosts []string `json:"hosts,omitempty"`
+
+	Gateways []string `json:"gateways,omitempty"`
+
+	Http []*HTTPRoute `json:"http,omitempty"`
+
+	Tls []*TLSRoute `json:"tls,omitempty"`
+
+	Tcp []*TCPRoute `json:"tcp,omitempty"`
+}
+
+type Destination struct {
+	Host string `json:"host,omitempty"`
+
+	Subset string `json:"subset,omitempty"`
+
+	Port *PortSelector `json:"port,omitempty"`
+}
+
+type HTTPRoute struct {
+	Match []*HTTPMatchRequest `json:"match,omitempty"`
+
+	Route []*DestinationWeight `json:"route,omitempty"`
+
+	Redirect *HTTPRedirect `json:"redirect,omitempty"`
+
+	Rewrite *HTTPRewrite `json:"rewrite,omitempty"`
+
+	WebsocketUpgrade bool `json:"websocketUpgrade,omitempty"`
+
+	Timeout string `json:"timeout,omitempty"`
+
+	Retries *HTTPRetry `json:"retries,omitempty"`
+
+	Mirror *Destination `json:"mirror,omitempty"`
+
+	CorsPolicy *CorsPolicy `json:"corsPolicy,omitempty"`
+
+	AppendHeaders map[string]string `json:"append_headers,omitempty"`
+
+	RemoveResponseHeaders []string `json:"removeResponseHeaders,omitempty"`
+}
+
+type TLSRoute struct {
+	Match []*TLSMatchAttributes `json:"match,omitempty"`
+
+	Route []*DestinationWeight `json:"route,omitempty"`
+}
+
+type TCPRoute struct {
+	Match []*L4MatchAttributes `json:"match,omitempty"`
+
+	Route []*DestinationWeight `json:"route,omitempty"`
+}
+
+type HTTPMatchRequest struct {
+	Uri *StringMatch `json:"uri,omitempty"`
+
+	Scheme *StringMatch `json:"scheme,omitempty"`
+
+	Method *StringMatch `json:"method,omitempty"`
+
+	Authority *StringMatch `json:"authority,omitempty"`
+
+	Headers map[string]*StringMatch `json:"headers,omitempty"`
+
+	Port uint32 `json:"port,omitempty"`
+
+	SourceLabels map[string]string `json:"sourceLabels"`
+
+	Gateways []string `json:"gateways,omitempty"`
+}
+
+type DestinationWeight struct {
+	Destination *Destination `json:"destination,omitempty"`
+
+	Weight int32 `json:"weight,omitempty"`
+}
+
+type L4MatchAttributes struct {
+	DestinationSubnets []string `json:"destinationSubnets,omitempty"`
+
+	Port uint32 `json:"port,omitempty"`
+
+	SourceSubnet string `json:"sourceSubnet,omitempty"`
+
+	SourceLabels map[string]string `json:"sourceLabels,omitempty"`
+
+	Gateways []string `json:"gateways,omitempty"`
+}
+
+type TLSMatchAttributes struct {
+	SniHosts []string `json:"sniHosts,omitempty"`
+
+	DestinationSubnets []string `json:"destinationSubnets,omitempty"`
+
+	Port uint32 `json:"port,omitempty"`
+
+	SourceSubnet string `json:"sourceSubnet,omitempty"`
+
+	SourceLabels map[string]string `json:"sourceLabels,omitempty"`
+
+	Gateways []string `json:"gateways,omitempty"`
+}
+
+type HTTPRedirect struct {
+	Uri string `json:"uri,omitempty"`
+
+	Authority string `json:"authority,omitempty"`
+}
+
+type HTTPRewrite struct {
+	Uri string `json:"uri,omitempty"`
+
+	Authority string `json:"authority,omitempty"`
+}
+
+type StringMatch struct {
+	Exact  *string `json:"exact,omitempty"`
+	Prefix *string `json:"prefix,omitempty"`
+	Regex  *string `json:"regex,omitempty"`
+}
+
+type HTTPRetry struct {
+	Attempts int32 `json:"attempts,omitempty"`
+
+	PerTryTimeout string `json:"perTryTimeout,omitempty"`
+}
+
+type CorsPolicy struct {
+	AllowOrigin []string `json:"allowOrigin,omitempty"`
+
+	AllowMethods []string `json:"allowMethods,omitempty"`
+
+	AllowHeaders []string `json:"allowHeaders,omitempty"`
+
+	ExposeHeaders []string `json:"exposeHeaders,omitempty"`
+
+	MaxAge string `json:"maxAge,omitempty"`
+
+	AllowCredentials bool `json:"allowCredentials,omitempty"`
+}
+
+type PortSelector struct {
+	Number *int32 `json:"number,omitempty"`
 }
